5b7eb1db6ecdb269b91c5abd4bc830f041030109
David Kabamba <david.kabamba@gmail.com>
2019-08-29 03:47:30 +0200

    add postlist and postlistItem components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet-blog';
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a leo sed odio pretium lobortis 
+      eu in ex. Maecenas accumsan pellentesque sem et vulputate. `,
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+        content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a leo sed odio pretium lobortis 
+          eu in ex. Maecenas accumsan pellentesque sem et vulputate. `,
+      loveIts: 0,
+      created_at: new Date()
+    },
+
+  ]
+
 }
